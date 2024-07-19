7bc60a7255fa78ec353d3e5090ceccbedd90cad6
Anish Saharan <111068771+anishsaharan@users.noreply.github.com>
2024-07-19 06:50:32 +0000

    Done

---
+++ Basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 123
+let accountName = "Anish"
+let accountPassword = "101010"
+accountCity = "Haryana"
+let accountState;
+
+
+/*
+- Constant Values Can't be Change
+- Use (Let) Instead of (var)
+- We can Declare Variable with Let, Var but we should not do that
+*/
+
+console.table([accountId, accountName, accountPassword, accountCity, accountState])
