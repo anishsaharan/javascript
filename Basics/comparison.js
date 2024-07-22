efe9af3faba4986a2838bccbca4f355dc384fdc1
Anish Saharan <111068771+anishsaharan@users.noreply.github.com>
2024-07-22 10:52:43 +0000

    comparison

---
+++ Basics/comparison.js
@@ -0,0 +1,15 @@
+//** All Comparision like this will return True/False
+
+// console.log(2>=3);
+// console.log(2==3);
+// console.log(2>3);
+// console.log(2<3);
+
+//** Data-Type Should be same for Comparision (MANDATORY)
+
+//**  === Check Data-type as Well
+
+console.log("2" == 2);          // Will Return True
+console.log("2" === 2);         // Will Return False
+
+//**  Avoide Comparison of Null and Undefined as they Occur Confusion in Data Converstion
